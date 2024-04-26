3cbd6f56bae4a5b403e0b9f16a32e71b0b7e8a03
Newt <guoching130@gmail.com>
2024-04-26 19:40:44 +0800

    sad

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,24 +1,29 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
+	"nvim-neo-tree/neo-tree.nvim",
 
-    opts = {
+	opts = {
 		close_if_last_window = true,
-        filesystem = {
-            filtered_items = {
-                hide_dotfiles = false,
-                hide_gitignored = false,
-                hide_hidden = false,
-            },
-        },
-        window = {
-          position = "right",
-          width = 50,
-        }
-    },
-    keys = {
-        {
-            "<C-n>",
-            "<cmd>Neotree toggle<cr>",
-        },
-    },
+		filesystem = {
+			filtered_items = {
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				hide_hidden = false,
+			},
+		},
+		window = {
+			position = "right",
+			width = 50,
+		},
+		actions = {
+			open_file = {
+				quit_on_open = true,
+			}
+		},
+	},
+	keys = {
+		{
+			"<C-n>",
+			"<cmd>Neotree toggle<cr>",
+		},
+	}
 }
