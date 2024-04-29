6f5b14cd402077ae6da62c1bc25f92a67c9605b3
Newt6611 <guoching130@gmail.com>
2024-04-29 16:25:00 +0800

    asd

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,25 +1,13 @@
 return {
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
-        },
-    },
-    {
-        "catppuccin/nvim",
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-                transparent_background = true,
-                term_colors = true,
-                -- color_overrides = {
-                --     mocha = {
-                --         base = "#000000",
-                --         mantle = "#000000",
-                --         crust = "#000000",
-                --     },
-                -- },
-            })
-        end,
-    }
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		}
+	}
 }
