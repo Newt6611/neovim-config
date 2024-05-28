f587673bde25f3abb232e58aa67d32bb331e1988
Newt6611 <guoching130@gmail.com>
2024-05-28 18:51:49 +0800

    asd

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,23 @@
+return {
+	'stevearc/oil.nvim',
+	opts = {
+		view_options = {
+			show_hidden = true
+		}
+	},
+
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	-- ["-"] = "actions.parent",
+	-- ["_"] = "actions.open_cwd",
+	keys = function()
+		return {
+			{ "-", function ()
+				require("oil").toggle_float()
+			end},
+
+			{ "<space>-", "<CMD>Oil<CR>", { desc = "Open parent directory" }}
+		}
+	end
+}
