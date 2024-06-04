b6cd00b5be75d4d5895257d03bde0265826f4d35
Newt6611 <guoching130@gmail.com>
2024-06-05 02:12:01 +0800

    asd

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,8 @@ return {
 	keys = function()
 		return {
 			{ "<leader>dv", "<cmd>Gvdiffsplit!<cr>" },
+			{ "<leader>dl", "<cmd>diffget //2<cr>" },
+			{ "<leader>dr", "<cmd>diffget //3<cr>" },
 		}
 	end,
 }
